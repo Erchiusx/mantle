2d9055e9050e4e64bebafea356f70db6db847a9c
Erchiusx <erchius_jzy@mail.ustc.edu.cn>
2025-04-09 17:24:23 +0800

    complete parser

--- src/Language/MantLe/Parser.hs
+++ src/Language/MantLe/Parser.hs
@@ -1,1 +1,27 @@
 module Language.MantLe.Parser where
+
+import Language.MantLe.Parser.Statements.Binding ( Binding )
+import Language.MantLe.Parser.Statements.Class ( Class )
+import Language.MantLe.Parser.Statements.Data ( Data )
+import Language.MantLe.Parser.Statements.Declare ( Declare )
+import Language.MantLe.Parser.Statements.Equation ( Equation )
+import Language.MantLe.Parser.Statements.Import ( Import )
+import Language.MantLe.Parser.Statements.Object ( Object )
+import Language.MantLe.Parser.Types ( Statement(..), Parser )
+import Text.Parsec (choice, many, try)
+
+data Stmt = forall a. Statement a => Stmt a
+
+file :: Parser u [Stmt]
+file =
+  many $
+    choice $
+      map try $
+        [ Stmt <$> expect @Import
+        , Stmt <$> expect @Object
+        , Stmt <$> expect @Class
+        , Stmt <$> expect @Data
+        , Stmt <$> expect @Declare
+        , Stmt <$> expect @Binding
+        , Stmt <$> expect @Equation
+        ]
